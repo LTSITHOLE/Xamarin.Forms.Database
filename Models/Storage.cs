bdd990f179ecbce29b5fb9d5940ef9bcb346a052
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2021-03-22 19:54:58 +0200

    Storage class added

---
+++ Models/Storage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.Forms.Database.Models
+{
+    /// <summary>
+    /// class to manage storage
+    /// </summary>
+    /// <typeparam name="T">type of Objet</typeparam>
+    public class Storage<T>
+    {
+    }
+}
